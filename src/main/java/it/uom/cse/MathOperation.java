8bc2162b90dce982624d72740bdd92625e02d0c7
Hiruna Harankahadeniya <hiruna.19@cse.mrt.ac.lk>
2021-09-23 19:16:55 +0530

    CSELIBRARY: fibonacci implementation

--- src/main/java/it/uom/cse/MathOperation.java
+++ src/main/java/it/uom/cse/MathOperation.java
@@ -15,4 +15,10 @@ public class MathOperation {
         return operand1 - operand2;
     }
 
+    static int fib(int n)
+    {
+        if (n <= 1)
+            return n;
+        return fib(n-1) + fib(n-2);
+    }
 }
